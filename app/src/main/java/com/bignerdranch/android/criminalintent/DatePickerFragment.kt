975169c21bdb4be998ec5ec0f12e70aabae9e795
curion4 <curion4@gmail.com>
2020-02-20 08:09:35 -0500

    Chapter 13 Challenge  adding time dialog

--- app/src/main/java/com/bignerdranch/android/criminalintent/DatePickerFragment.kt
+++ app/src/main/java/com/bignerdranch/android/criminalintent/DatePickerFragment.kt
@@ -2,10 +2,12 @@ package com.bignerdranch.android.criminalintent
 
 import android.app.DatePickerDialog
 import android.app.Dialog
+import android.app.TimePickerDialog
 import android.os.Bundle
 import android.widget.DatePicker
 import androidx.fragment.app.DialogFragment
 import java.util.*
+import java.util.Calendar.*
 
 private const val ARG_DATE = "date"
 class DatePickerFragment : DialogFragment() {
@@ -15,21 +17,25 @@ class DatePickerFragment : DialogFragment() {
     }
 
     override fun onCreateDialog(savedInstanceState: Bundle?): Dialog {
-        val dateListener = DatePickerDialog.OnDateSetListener {
-                _: DatePicker, year: Int, month: Int, day: Int ->
-
-            val resultDate: Date = GregorianCalendar(year, month, day).time
-
-            targetFragment?.let { fragment ->
-                (fragment as Callbacks).onDateSelected((resultDate))
-            }
-        }
         val date: Date = arguments?.getSerializable(ARG_DATE) as Date
-        val calendar = Calendar.getInstance()
+        val calendar = getInstance()
         calendar.time = date
-        val initialYear = calendar.get(Calendar.YEAR)
-        val initialMonth = calendar.get(Calendar.MONTH)
-        val initialDay = calendar.get(Calendar.DAY_OF_MONTH)
+        val initialYear = calendar.get(YEAR)
+        val initialMonth = calendar.get(MONTH)
+        val initialDay = calendar.get(DAY_OF_MONTH)
+        val initialHour = calendar.get(HOUR_OF_DAY)
+        val initialMinute = calendar.get(MINUTE)
+
+        val dateListener = DatePickerDialog.OnDateSetListener {_, year, month, day ->
+            TimePickerDialog(requireContext(), TimePickerDialog.OnTimeSetListener{ _, hour, minute ->
+                val pickedDateTime = getInstance()
+                pickedDateTime.set(year, month, day, hour, minute)
+                targetFragment?.let { fragment ->
+                    (fragment as Callbacks).onDateSelected(pickedDateTime.time)
+                }
+            }, initialHour, initialMinute, false).show()
+        }
+
         return DatePickerDialog(
             requireContext(),
             dateListener,
