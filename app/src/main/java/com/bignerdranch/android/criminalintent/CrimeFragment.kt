8987fc53dd7a76a5c3abb3ad178d0452e6bb9752
curion4 <curion4@gmail.com>
2020-08-20 22:50:54 -0400

    Revert "Added a more efficient thumbnail load using ViewTreeObserver"
    This reverts commit 40b6c7d4b9b117a70b73458f6070faccd28c24d4.

--- app/src/main/java/com/bignerdranch/android/criminalintent/CrimeFragment.kt
+++ app/src/main/java/com/bignerdranch/android/criminalintent/CrimeFragment.kt
@@ -14,7 +14,6 @@ import android.text.format.DateFormat
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.view.ViewTreeObserver
 import android.widget.*
 import androidx.core.content.FileProvider
 import androidx.fragment.app.Fragment
@@ -43,8 +42,6 @@ class CrimeFragment : Fragment(), DatePickerFragment.Callbacks {
     private lateinit var suspectButton: Button
     private lateinit var photoButton: ImageButton
     private lateinit var photoView: ImageView
-    private var height = 0
-    private var width = 0
     private val crimeDetailViewModel: CrimeDetailViewModel by lazy {
         ViewModelProvider(this).get(CrimeDetailViewModel::class.java)
     }
@@ -71,15 +68,6 @@ class CrimeFragment : Fragment(), DatePickerFragment.Callbacks {
         photoButton = view.findViewById(R.id.crime_camera) as ImageButton
         photoView = view.findViewById(R.id.crime_photo) as ImageView
 
-        photoView.viewTreeObserver.addOnGlobalLayoutListener {
-            object : ViewTreeObserver.OnGlobalLayoutListener {
-                override fun onGlobalLayout() {
-                    photoView.viewTreeObserver.removeOnGlobalLayoutListener(this);
-                    updatePhotoView()
-                }
-            }
-        }
-
         return view
     }
 
@@ -94,7 +82,7 @@ class CrimeFragment : Fragment(), DatePickerFragment.Callbacks {
                     photoUri = FileProvider.getUriForFile(requireActivity(),
                         "com.bignerdranch.android.criminalintent.fileprovider",
                         photoFile)
-                    updatePhotoView()
+                    updateUI()
                 }
             })
     }
@@ -258,16 +246,12 @@ class CrimeFragment : Fragment(), DatePickerFragment.Callbacks {
         if(crime.suspect.isNotEmpty()) {
             suspectButton.text = crime.suspect
         }
-
-        if (height != 0 && width != 0) {
-            updatePhotoView()
-        }
+        updatePhotoView()
     }
 
     private fun updatePhotoView() {
         if (photoFile.exists()) {
-            val bitmap = getScaledBitmap(photoFile.path, photoView.measuredWidth, photoView.measuredHeight)
-
+            val bitmap = getScaledBitmap(photoFile.path, requireActivity())
             photoView.setImageBitmap(bitmap)
         } else {
             photoView.setImageDrawable(null)
